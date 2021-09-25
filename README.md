250d8c707e1df4c86dcababa5d9addb03149bfdc
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-09-25 23:44:25 +0600

    header done

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+
 
 <ul>
 <li>Bullet point</li>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div className="text-center header">
+            <h1>Make a Programmer Team</h1>
+            <p>Programming isn't about what you know; it's about what you can figure out.</p>
+            <h1>Total Budget: 50+ Million</h1>
+        </div>
+    );
+};
+
+export default Header;
